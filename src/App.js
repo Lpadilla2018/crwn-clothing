522841a6efc8f42feb646986387fcf707b62fb28
L-Train-0101 <louie@revunit.com>
2022-03-01 14:26:03 -0700

    Add google firebase utils, add email and pass signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 // Pages
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 // Components
-import Header from './components/header/header.component.jsx';
+import Header from "./components/header/header.component.jsx";
 
 class App extends React.Component {
   constructor() {
@@ -32,17 +32,13 @@ class App extends React.Component {
         (await userRef).onSnapshot((snapShot) => {
           if (snapShot) {
             // set state of current user
-            this.setState(
-              {
-                currentUser: {
-                  id: snapShot.id,
-                  ...snapShot.data(),
-                },
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
               },
-              () => {
-                console.log(this.state);
-              }
-            );
+            });
+            console.log(this.state);
           } else {
             // Set to null
             this.setState({ currentUser: userAuth });
@@ -61,9 +57,9 @@ class App extends React.Component {
       <div>
         <Header currentUser={this.state.currentUser} />
         <Routes>
-          <Route path='/' element={<HomePage />}></Route>
-          <Route path='/shop' element={<ShopPage />}></Route>
-          <Route path='/signin' element={<SignInAndSignUpPage />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/shop" element={<ShopPage />}></Route>
+          <Route path="/signin" element={<SignInAndSignUpPage />}></Route>
         </Routes>
       </div>
     );
